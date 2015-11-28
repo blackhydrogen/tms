36ae9c031d300b75fa826b854a6393a2a9656766
Ian <blackhydrogen@users.noreply.github.com>
2015-11-28 08:47:08 +0800

    Initial commit for 0.1 of STReader

---
+++ STReader.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         STReader
+// @namespace    https://github.com/blackhydrogen/tms
+// @version      0.1
+// @description  Allows you to read Straits Times articles anytime.
+// @author       BH
+// @match        http://www.straitstimes.com/*
+// @grant        none
+// ==/UserScript==
+/* jshint -W097 */
+'use strict';
+
+// Your code here...
+if(document.getElementById("hardPaywallModal")) {
+    document.body.style.overflow = "visible";
+    document.getElementsByClassName("modal-backdrop")[0].remove();
+    document.getElementById("hardPaywallModal").remove();
+}
