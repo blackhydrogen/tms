7df91f4678fc459df7f165f7226fe3d1b8954448
Ian Leow <blackhydrogen@users.noreply.github.com>
2016-01-20 22:54:43 +0800

    Update THU.user.js

--- THU.user.js
+++ THU.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Telegram Hide Unread
 // @namespace    https://github.com/blackhydrogen/tms
-// @version      5
+// @version      6
 // @description  Hides the unread icon (blue dot).
 // @author       BH
 // @match        https://web.telegram.org/*
@@ -10,5 +10,5 @@
 /* jshint -W097 */
 'use strict';
 
-document.styleSheets[0].addRule(".im_dialog_unread", "visibility:hidden");
-document.styleSheets[0].addRule(".icon-message-status", "visibility:hidden");
+document.styleSheets[0].addRule(".im_dialog_unread", "{ visibility: hidden }");
+document.styleSheets[0].addRule(".icon-message-status", "{ visibility: hidden }");
